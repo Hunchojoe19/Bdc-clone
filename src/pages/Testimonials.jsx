d41a6ef7668990bb3981cfec2d5c74eaf63c62e6
Hunchojoe19 <enubiakjoseph@gmail.com>
2025-01-27 17:45:01 +0100

    changed some things in the code

--- src/pages/Testimonials.jsx
+++ src/pages/Testimonials.jsx
@@ -4,24 +4,23 @@ const Testimonials = () => {
   const testimonials = [
     {
       rating: "★★★★★",
-      text: "Bitcoin.com made it so easy to buy crypto. The user interface is simple, and I love how quickly my transactions go through!",
+      text: "CosCoin.info made it so easy to buy crypto. The user interface is simple, and I love how quickly my transactions go through!",
       author: "Samuel M",
     },
     {
       rating: "★★★★",
-      text: "Bitcoin.com has transformed my experience with cryptocurrency! The platform is user-friendly and my transactions are processed in no time.",
+      text: "CosCoin.info has transformed my experience with cryptocurrency! The platform is user-friendly and my transactions are processed in no time.",
       author: "Rachel K",
     },
     {
       rating: "★★★★★",
-      text: "Thanks to Bitcoin.com, I feel confident investing in crypto. The website is intuitive, and my transactions happen instantly!",
+      text: "Thanks to CosCoin.info, I feel confident investing in crypto. The website is intuitive, and my transactions happen instantly!",
       author: "Rahim M",
     },
   ];
 
   return (
-    <section className="bg-gray-800 text-white py-12" data-aos="flip-left" data-aos-easing="ease-in-out"
-     data-aos-duration="1500">
+    <section className="bg-gray-800 text-white py-12">
       <div className="container mx-auto px-4">
         <h2 className="text-3xl md:text-4xl font-bold text-center mb-8">
           Join the growing community of users who trust our platform
